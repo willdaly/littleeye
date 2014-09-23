b3d84410aef37ea4609bb9e220e9c50cabe6d82a
Will Daly <rwilliamdaly@gmail.com>
2014-09-23 10:35:42 -0500

    fixed typo

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,6 +4,6 @@ Instagram.configure do |config|
 
   config.client_id = ENV["INSTAGRAM_CLIENT_ID"]
 
-  config.access_token = ENV["INSTAGRAM_CLIENT_ID"]
+  config.access_token = ENV["INSTAGRAM_ACCESS_TOKEN"]
 
 end
