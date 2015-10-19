135fce5b98478426157cb6ad50e79d3fcd24b6ca
Will Daly <rwilliamdaly@gmail.com>
2015-10-19 17:05:29 -0500

    user removed validatable replaced with omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :omniauthable
   has_many :missions
   validates_numericality_of :score
 end
