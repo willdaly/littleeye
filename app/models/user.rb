0c4168eb12280cf7fb04b857e323d04c9adc0673
Will Daly <rwilliamdaly@gmail.com>
2015-10-19 17:53:58 -0500

    changed resource class to devise since class was undefined because partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauthable
   has_many :missions
