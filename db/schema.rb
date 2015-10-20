e4eb14f9466dde6668dad0fb9c7819ae872ccf72
Will Daly <rwilliamdaly@gmail.com>
2015-10-19 21:51:05 -0500

    remove email from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020013522) do
+ActiveRecord::Schema.define(version: 20151020025009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20151020013522) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                             default: "", null: false
     t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -49,7 +48,6 @@ ActiveRecord::Schema.define(version: 20151020013522) do
     t.string   "name",                   limit: 30
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
