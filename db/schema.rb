405abb736b716dcc5e95001e8d53fb9ecd6d9bb1
Will Daly <rwilliamdaly@gmail.com>
2015-10-19 20:23:27 -0500

    added provider and uid to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921111137) do
+ActiveRecord::Schema.define(version: 20151020012034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,13 @@ ActiveRecord::Schema.define(version: 20140921111137) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "score",                  default: 0
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
 end
