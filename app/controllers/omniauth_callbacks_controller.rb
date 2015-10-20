1d6eaf6b01c7526ffc2d4f6e97370a28a5bfcd90
Will Daly <rwilliamdaly@gmail.com>
2015-10-19 21:39:39 -0500

    removed users/ from devise for in routes and unsubclassed callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def instagram
     @user = User.find_or_create_by_instagram_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
